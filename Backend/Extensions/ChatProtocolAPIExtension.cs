9e3456056b108ce2b3e360c69854840a60e3213e
bradygaster <bradyg@microsoft.com>
2024-02-14 11:10:51 -0800

    starting support of the chat protocol

---
+++ Backend/Extensions/ChatProtocolAPIExtension.cs
@@ -0,0 +1,24 @@
+﻿namespace Microsoft.AspNetCore.Builder;
+
+public static class ChatProtocolAPIExtension
+{
+    public static IEndpointRouteBuilder AddChatProtocolApis(this IEndpointRouteBuilder builder)
+    {
+        builder.MapGet("/", () =>
+        {
+
+        });
+
+        builder.MapPost("chat", () =>
+        {
+
+        });
+
+        builder.MapPost("ask", () =>
+        { 
+
+        });
+
+        return builder;
+    }
+}
