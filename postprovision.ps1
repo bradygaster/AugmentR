62754253b22c96c5d3910141f3afc2f1ec18fbd6
Jon Gallant <2163001+jongio@users.noreply.github.com>
2024-03-04 20:54:01 +0000

    Update to support = in vals

--- postprovision.ps1
+++ postprovision.ps1
@@ -5,9 +5,18 @@ function Set-DotnetUserSecrets {
     dotnet user-secrets init
     dotnet user-secrets clear
     foreach ($line in $lines) {
-        $name, $value = $line -split '='
+        # Split the line at the first equal sign only
+        $parts = $line -split '=', 2
+        $name = $parts[0]
+        $value = $parts[1]
+
+        # Remove quotes from the value
         $value = $value -replace '"', ''
+        
+        # Replace double underscores with colon in the name
         $name = $name -replace '__', ':'
+
+        # Set the secret if the value is not empty
         if ($value -ne '') {
             dotnet user-secrets set $name $value | Out-Null
         }
